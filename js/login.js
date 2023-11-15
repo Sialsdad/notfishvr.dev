d469e722070e51e44803e005e49b9ae4df796138
notfishvr <notfishvrvr+6545@gmail.com>
2023-11-15 03:05:43 -0500

    a
    a

--- js/login.js
+++ js/login.js
@@ -2,42 +2,34 @@ var allUsers = [];
 var users = localStorage.getItem("users");
 var currentUser = localStorage.getItem("currentUser");
 
-
 if (users !== null) {
     allUsers = JSON.parse(users);
 }
 
-
-
 function login() {
-    var email = document.querySelector("#lemail").value;
-    var password = document.querySelector("#lpass").value;
+    var password = document.querySelector("#lname").value;
+    var name = document.querySelector("#lpass").value;
     var errorPara = document.querySelector("#errorPara");
-
-    if (email == "" && password == "") {
-        errorPara.innerHTML = "Please fill all the inputs!";
+    
+    if (name == "") {
+        errorPara.innerHTML = "Please fill the name input!";
         setTimeout(() => {
             errorPara.innerHTML = "";
         }, 4000);
-    } else if (email == "") {
-        errorPara.innerHTML = "Please fill the email input!";
-        setTimeout(() => {
-            errorPara.innerHTML = "";
-        }, 4000);
-    } else if (password == "") {
+    }
+    else if (password == "") {
         errorPara.innerHTML = "Please fill the password input!";
         setTimeout(() => {
             errorPara.innerHTML = "";
         }, 4000);
     } else {
-
-
         var filterUser = allUsers.filter(function (data) {
-            return data.email == email && data.password == password && data.name
+            return data.name == name && data.password == password;
         });
+
         if (filterUser.length) {
             localStorage.setItem("currentUser", JSON.stringify(filterUser));
-            location.href = "../index.html"
+            location.href = "../index.html";
         } else {
             alert("Please signup first!");
             location.href = "../signup/signup.html";
