689dd5e3fba3482d5b7eeb139f9e238f7007bbc2
notfishvr <notfishvrvr+6545@gmail.com>
2023-11-15 03:18:10 -0500

    a

--- js/login.js
+++ js/login.js
@@ -35,36 +35,3 @@ function login() {
         }
     }
 }
-function makeacc() {
-    var name = document.querySelector("#sname").value;
-    var password = document.querySelector("#spass").value;
-    var errorPara = document.querySelector("#errorPara");
-
-    if (name == "" && password == "") {
-        errorPara.innerHTML = "Please fill all the inputs!";
-        setTimeout(() => {
-            errorPara.innerHTML = "";
-        }, 4000);
-    } else if (name == "") {
-        errorPara.innerHTML = "Please fill the name input!";
-        setTimeout(() => {
-            errorPara.innerHTML = "";
-        }, 4000);
-    } else if (password == "") {
-        errorPara.innerHTML = "Please fill the password input!";
-        setTimeout(() => {
-            errorPara.innerHTML = "";
-        }, 4000);
-    } else {
-
-        var singleData = {
-            name: name,
-            password: password
-        }
-
-        allUsers.push(singleData);
-        localStorage.setItem("users", JSON.stringify(allUsers));
-        document.querySelector("#sname").value = "";
-        document.querySelector("#spass").value = "";
-    }
-}
