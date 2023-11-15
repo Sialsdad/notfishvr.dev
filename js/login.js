01f9a228ff067831f17b7ac4246d18aa5cc2c1ef
notfishvr <notfishvrvr+6545@gmail.com>
2023-11-15 03:33:01 -0500

    a

--- js/login.js
+++ js/login.js
@@ -22,41 +22,31 @@ function login() {
             errorPara.innerHTML = "";
         }, 4000);
     } else {
-        // Use fingerprintjs2 to get the hardware ID
-        new Fingerprint2().get(function (hwid) {
-            var storedHWID = localStorage.getItem("userHWID");
+        fetch('https://api64.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                var userIP = data.ip;
 
-            if (storedHWID === null || storedHWID === hwid) {
-                fetch('https://api64.ipify.org?format=json')
-                    .then(response => response.json())
-                    .then(data => {
-                        var userIP = data.ip;
+                var storedIP = localStorage.getItem("userIP");
 
-                        var storedIP = localStorage.getItem("userIP");
-
-                        if (storedIP === null || storedIP === userIP) {
-                            var filterUser = allUsers.filter(function (data) {
-                                return data.name == name && data.password == password;
-                            });
-
-                            if (filterUser.length) {
-                                localStorage.setItem("userIP", userIP);
-                                localStorage.setItem("userHWID", hwid);
-                                localStorage.setItem("currentUser", JSON.stringify(filterUser));
-                                location.href = "../index.html";
-                            } else {
-                                alert("Invalid username or password");
-                            }
-                        } else {
-                            alert("Access denied. IP address mismatch.");
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error fetching IP address:', error);
+                if (storedIP === null || storedIP === userIP) {
+                    var filterUser = allUsers.filter(function (data) {
+                        return data.name == name && data.password == password;
                     });
-            } else {
-                alert("Access denied. Hardware ID mismatch.");
-            }
-        });
+
+                    if (filterUser.length) {
+                        localStorage.setItem("userIP", userIP);
+                        localStorage.setItem("currentUser", JSON.stringify(filterUser));
+                        location.href = "../index.html";
+                    } else {
+                        alert("Invalid username or password");
+                    }
+                } else {
+                    alert("Access denied. IP address mismatch.");
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching IP address:', error);
+            });
     }
 }
