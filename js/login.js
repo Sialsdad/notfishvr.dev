4043fd2c3168af87cc203c4114ad7afb97a0e0ce
notfishvr <notfishvrvr+6545@gmail.com>
2023-11-15 03:36:57 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,43 +10,28 @@ function login() {
     var password = document.querySelector("#lname").value;
     var name = document.querySelector("#lpass").value;
     var errorPara = document.querySelector("#errorPara");
-
+    
     if (name == "") {
         errorPara.innerHTML = "Please fill the name input!";
         setTimeout(() => {
             errorPara.innerHTML = "";
         }, 4000);
-    } else if (password == "") {
+    }
+    else if (password == "") {
         errorPara.innerHTML = "Please fill the password input!";
         setTimeout(() => {
             errorPara.innerHTML = "";
         }, 4000);
     } else {
-        fetch('https://api64.ipify.org?format=json')
-            .then(response => response.json())
-            .then(data => {
-                var userIP = data.ip;
+        var filterUser = allUsers.filter(function (data) {
+            return data.name == name && data.password == password;
+        });
 
-                var storedIP = localStorage.getItem("userIP");
-
-                if (storedIP === null || storedIP === userIP) {
-                    var filterUser = allUsers.filter(function (data) {
-                        return data.name == name && data.password == password;
-                    });
-
-                    if (filterUser.length) {
-                        localStorage.setItem("userIP", userIP);
-                        localStorage.setItem("currentUser", JSON.stringify(filterUser));
-                        location.href = "../index.html";
-                    } else {
-                        alert("Invalid username or password");
-                    }
-                } else {
-                    alert("Access denied. IP address mismatch.");
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching IP address:', error);
-            });
+        if (filterUser.length) {
+            localStorage.setItem("currentUser", JSON.stringify(filterUser));
+            location.href = "../index.html";
+        } else {
+            alert("no work");
+        }
     }
 }
