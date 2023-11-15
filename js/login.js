49cf7837318f6443a592f2e731706ebb1b27105e
notfishvr <notfishvrvr+6545@gmail.com>
2023-11-15 02:21:46 -0500

    a
    a

--- js/login.js
+++ js/login.js
@@ -1,13 +1,39 @@
-import { sha256 } from "https://cdn.skypack.dev/js-sha256";
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to handle the login button click
+    document.getElementById('login-button').addEventListener('click', function () {
+        // Get the values from the input fields
+        var license = document.getElementById('login-license').value;
+        var password = document.getElementById('login-password').value;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginLicense = document.querySelector("#login-license");
-    const loginPassword = document.querySelector("#login-password");
+        // Validate the license and password (you should replace this with your actual validation logic)
+        if (license.trim() === '' || password.trim() === '') {
+            alert('Please enter both license and password.');
+            return;
+        }
 
-    const loginButton = document.querySelector("#login-button");
-
-    loginButton.addEventListener("click", () => {
-        const licenseHash = sha256(loginLicense.value);
-        const passwordHash = sha256(loginPassword.value + licenseHash);
+        // Here you should make an AJAX request to your server to validate the login credentials
+        // Replace the following code with your actual server-side logic
+        // Example: assume there's a server endpoint '/login' that validates the credentials
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ license: license, password: password }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Check the response from the server
+                if (data.success) {
+                    alert('Login successful!');
+                    // Redirect to a new page or perform other actions after successful login
+                } else {
+                    alert('Login failed. Please check your credentials.');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('An error occurred during login.');
+            });
     });
 });
