b74d09eba2dfd0cb5f8e799a923cf3cb1853ebe5
notfishvr <124737490+official-notfishvr@users.noreply.github.com>
2023-11-29 20:28:58 -0500

    Add files via upload

--- ZenX/login.js
+++ ZenX/login.js
@@ -1,18 +1,4 @@
 function loginStart() {
-    document.getElementById('captcha').style.display = 'none';
     document.getElementById('loginBtn').style.display = 'none';
     document.getElementById('loginBtn').style.backgroundColor = '#151515';
-    document.getElementById('captchaBtn').style.display = '';
-}
-
-function captchaOpen() {
-    document.getElementById('captcha').style.display = '';
-    document.getElementById('loginBtn').style.display = '';
-    document.getElementById('captchaBtn').style.display = 'none';
-}
-
-function captchaClose() {
-    document.getElementById('captcha').style.display = 'none';
-    document.getElementById('loginBtn').style.display = 'none';
-    document.getElementById('captchaBtn').style.display = '';
 }
